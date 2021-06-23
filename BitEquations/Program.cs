ce019e26b1d1086e80a2e1f902f043e4d96a5698
ivan malechkin <scorpunduk@yandex.ru>
2021-06-23 17:21:49 +0300

    00 INIT COMMIT

---
+++ BitEquations/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BitEquations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5, y = 10, z = 15;
+
+            Console.WriteLine($"x={x}, y={y}, z={z}");
+            Console.WriteLine(new string('.', 100));
+
+            x += y >> x++ * z;
+            Console.WriteLine("x += y >> x++ * z");
+            Console.WriteLine($"x={x}, y={y}, z={z}");
+            Console.WriteLine(new string('.', 100));
+
+            z = ++x & y * 5;
+            Console.WriteLine("z = ++x & y * 5");
+            Console.WriteLine($"x={x}, y={y}, z={z}");
+            Console.WriteLine(new string('.', 100));
+
+            y /= x + 5 | z;
+            Console.WriteLine("y /= x + 5 | z");
+            Console.WriteLine($"x={x}, y={y}, z={z}");
+            Console.WriteLine(new string('.', 100));
+
+            z = x++ & y * 5;
+            Console.WriteLine("z = x++ & y * 5");
+            Console.WriteLine($"x={x}, y={y}, z={z}");
+            Console.WriteLine(new string('.', 100));
+
+            x = y << x++ ^ z;
+            Console.WriteLine("x = y << x++ ^ z");
+            Console.WriteLine($"x={x}, y={y}, z={z}");
+            Console.WriteLine(new string('.', 100));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CS5_05/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CS5_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool process = true;
+
+            while (process)
+            {
+                Console.WriteLine("Введите два целых числа. " +
+                "Будут выведены все чётные числа\n" +
+                "в диапазоне между двумя заданными числами.\n" +
+                "Первое число:");
+
+                int startNumber = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Второе число");
+
+                int endNumber = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(new string('-', Console.WindowWidth) + "\n");
+
+                for (int i = startNumber; i <= endNumber; i++)
+                {
+                    int check = i & 1;
+                    if (check == 0) Console.WriteLine(i);
+                }
+
+                Console.WriteLine(new string('-', Console.WindowWidth) + "\n");
+
+                Console.WriteLine("Повторить? (да/нет)");
+                process = Console.ReadLine() == "да" ? true : false;
+
+                Console.Clear();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
